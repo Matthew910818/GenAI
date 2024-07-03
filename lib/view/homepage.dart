a386e6e1734de476e03b10085e803c87ee47eae7
Matthew910818 <matthew910818@gmail.com>
2024-07-03 22:22:10 +0800

    genai

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import '../viewmodel/viewmodel.dart';
 import 'highlight1.dart';
 import 'highlight2.dart';
@@ -28,7 +29,7 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            Image.asset('assets/dunk.png'),
+            VideoPlayerWidget(videoPath: 'assets/Finals.mp4'),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
@@ -138,5 +139,46 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class VideoPlayerWidget extends StatefulWidget {
+  final String videoPath;
+
+  VideoPlayerWidget({required this.videoPath});
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {
+          _isInitialized = true;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : Container(); // 或者顯示加載動畫或佔位符
   }
 }

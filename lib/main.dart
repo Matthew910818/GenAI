acf092d07532d0a44fde2bcd51038eaeda96088e
Matthew910818 <matthew910818@gmail.com>
2024-07-06 23:46:17 +0800

    home change

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(), // 改
-      //home: HomePage(),
+      //home: MyHomePage(), // 改
+      home: HomePage(),
     );
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,7 +3,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:genai_v2/view/video_page.dart';
 import '../viewmodel/video_vm.dart';
 import '../model/video.dart';
-import 'video_player.dart'; // Ensure this import points to your updated video player widget
+import 'video_player.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -27,7 +27,8 @@ class _HomePageState extends State<HomePage> {
   Future<void> _loadData() async {
     await viewModel.loadVideos();
     setState(() {
-      videoPaths = viewModel.videoHighlights.map((video) => video.videoPath).toList();
+      videoPaths =
+          viewModel.videoHighlights.map((video) => video.videoPath).toList();
       isLoading = false;
     });
   }
@@ -35,12 +36,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 35, 30, 30),
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 35, 30, 30),
-        elevation: 20,
+        backgroundColor: Colors.black,
+        elevation: 0,
         centerTitle: true,
-        title: Text('愛爾達', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white)),
+        title: Text('Gelaito4',
+            style: TextStyle(
+                fontFamily: 'Pacifico', fontSize: 30, color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.home, color: Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -48,36 +60,11 @@ class _HomePageState extends State<HomePage> {
               ? Center(child: CircularProgressIndicator())
               : SingleChildScrollView(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      CarouselSlider.builder(
-                        itemCount: videoPaths.length,
-                        options: CarouselOptions(
-                          autoPlay: false,
-                          aspectRatio: 2.0,
-                          enlargeCenterPage: true,
-                          onPageChanged: (index, reason) {
-                            setState(() {
-                              _current = index;
-                            });
-                          },
-                        ),
-                        itemBuilder: (context, index, realIndex) {
-                          return Container(
-                            width: constraints.maxWidth,
-                            child: Center(
-                              child: AspectRatio(
-                                aspectRatio: 16 / 9,
-                                child: VideoPlayerWidget(
-                                  videoPath: videoPaths[index],
-                                  isActive: index == _current,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
+                      _buildVideoPlayerSection(constraints),
                       SizedBox(height: 20),
-                      _buildRecommendedVideoSection(constraints),
+                      _buildRelatedVideosSection(),
                     ],
                   ),
                 );
@@ -86,59 +73,145 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildRecommendedVideoSection(BoxConstraints constraints) {
+  Widget _buildVideoPlayerSection(BoxConstraints constraints) {
+    return CarouselSlider.builder(
+      itemCount: videoPaths.length,
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
+        onPageChanged: (index, reason) {
+          setState(() {
+            _current = index;
+          });
+        },
+      ),
+      itemBuilder: (context, index, realIndex) {
+        return Container(
+          width: constraints.maxWidth,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: VideoPlayerWidget(
+                videoPath: videoPaths[index],
+                isActive: index == _current,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildVideoSummarySection() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+    );
+  }
+
+  Widget _buildVideoHighlightsSection() {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Text(
+            'Highlights',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 10),
+          _buildHighlightList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHighlightList() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 16 / 9,
+      ),
+      itemCount: viewModel.videoHighlights.length,
+      itemBuilder: (context, index) {
+        final video = viewModel.videoHighlights[index];
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+          },
+          child: _buildHighlightCard(video),
+        );
+      },
+    );
+  }
+
+  Widget _buildHighlightCard(Video video) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black54,
+            blurRadius: 5,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
             children: [
-              Text(
-                'Recommended',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                child: Image.asset(
+                  video.imagePath,
+                  width: double.infinity,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                right: 8,
+                bottom: 8,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+                  color: Colors.black54,
+                  child: Text(
+                    video.duration,
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
                 ),
               ),
             ],
           ),
-          SizedBox(height: 10),
-          Container(
-            height: 600,
-            child: GridView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: viewModel.recommendedVideos.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.75,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-              ),
-              itemBuilder: (context, index) {
-                final video = viewModel.recommendedVideos[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyHomePage()),
-                    );
-                  },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('channel_icon.png'),
+                ),
+                SizedBox(width: 8),
+                Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.asset(
-                          video.imagePath,
-                          width: double.infinity,
-                          height: 400,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 8),
                       Text(
                         video.title,
                         style: TextStyle(
@@ -148,10 +221,26 @@ class _HomePageState extends State<HomePage> {
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Channel Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white54,
+                        ),
+                      ),
+                      Text(
+                        '123K views • 1 day ago',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white54,
+                        ),
+                      ),
                     ],
                   ),
-                );
-              },
+                ),
+                Icon(Icons.more_vert, color: Colors.white),
+              ],
             ),
           ),
         ],
@@ -159,35 +248,50 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildRecommendCard(Video video) {
-    return Container(
-      width: double.infinity,
-      height: 100.0,
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-        color: Color(0xFF2C2F33), // Dark background color for the cards
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: ListTile(
-        leading: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.asset(
-            video.imagePath,
-            width: 60,
-            height: 60,
-            fit: BoxFit.cover,
+  Widget _buildRelatedVideosSection() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recommended Videos',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
-        ),
-        title: Text(
-          video.title,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
+          SizedBox(height: 10),
+          _buildRelatedVideoList(),
+        ],
       ),
+    );
+  }
+
+  Widget _buildRelatedVideoList() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 16 / 9,
+      ),
+      itemCount: viewModel.similarVideos.length,
+      itemBuilder: (context, index) {
+        final video = viewModel.similarVideos[index];
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+          },
+          child: _buildHighlightCard(video),
+        );
+      },
     );
   }
 }
